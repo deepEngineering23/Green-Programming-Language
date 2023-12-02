06d5b919e0664290d5c6a4baeb607e8f2f5b8dbd
Amartya Pandey <amartya.pandey23@gmail.com>
2023-12-02 15:20:08 +0530

    parsing function and expressions

--- lexer.cpp
+++ lexer.cpp
@@ -4,19 +4,203 @@
 
 #include "lexertk.hpp"
 
+using namespace std;
 
-int main()
+struct function_definition
 {
-   std::string expression = "(cos(x/pi)cos(2y) + 1) == (sin(x / pi) * cos(2 * y) + 1)";
+   std::string name;
+   std::string body;
+   std::vector<std::string> var_list;
+
+   void clear()
+   {
+      name    .clear();
+      body    .clear();
+      var_list.clear();
+   }
+};
+
+struct parse_function_definition_impl : public lexertk::parser_helper
+{
+   /*
+      Structure: function <name> (v0,v1,...,vn) { expression }
+   */
+   bool process(std::string& func_def, function_definition& fd)
+   {
+      if (!init(func_def))
+         return false;
+
+      if (!token_is(token_t::e_symbol,"function"))
+         return false;
+
+      if (!token_is_then_assign(token_t::e_symbol,fd.name))
+         return false;
+
+      if (!token_is(token_t::e_lbracket))
+         return false;
+
+      if (!token_is(token_t::e_rbracket))
+      {
+         std::vector<std::string> var_list;
+
+         for ( ; ; )
+         {
+            // (x,y,z,....w)
+            if (!token_is_then_assign(token_t::e_symbol,var_list))
+               return false;
+
+            if (token_is(token_t::e_rbracket))
+               break;
+
+            if (!token_is(token_t::e_comma))
+               return false;
+         }
+
+         var_list.swap(fd.var_list);
+      }
+
+      std::size_t body_begin = current_token().position;
+      std::size_t body_end   = current_token().position;
+
+      int bracket_stack = 0;
+
+      if (!token_is(token_t::e_lcrlbracket,e_hold))
+         return false;
+
+      for ( ; ; )
+      {
+         body_end = current_token().position;
+
+         if (token_is(token_t::e_lcrlbracket))
+            bracket_stack++;
+         else if (token_is(token_t::e_rcrlbracket))
+         {
+            if (0 == --bracket_stack)
+               break;
+         }
+         else
+         {
+            if (lexer().finished())
+               return false;
+
+            next_token();
+         }
+      }
+
+      std::size_t size = body_end - body_begin + 1;
+
+      fd.body = func_def.substr(body_begin,size);
+
+      const std::size_t index = body_begin + size;
+
+      if (index < func_def.size())
+         func_def = func_def.substr(index,func_def.size() - index);
+      else
+         func_def = "";
+
+      return true;
+   }
+};
+
+bool parse_function_definition(std::string& func_def, function_definition& fd)
+{
+   parse_function_definition_impl parser;
+   return parser.process(func_def,fd);
+}
+void parsingBody(string expression)
+{
+   //std::string expression = "{a+(b-[c*(e/{f+g}-h)*i]%[j+(k-{l*m}/n)+o]-p)*q}";
 
    lexertk::generator generator;
 
    if (!generator.process(expression))
    {
-      return 0;
+      std::cout << " Failed to lex: " << expression << std::endl;
+      return;
    }
 
+   lexertk::helper::bracket_checker bc;
+   bc.process(generator);
+
+   if (!bc.result())
+   {
+      std::cout << "Failed Bracket Checker!" << std::endl;
+      return;
+   }
+
+   std::cout << "***** actual expression parsing*****" << std::endl;
    lexertk::helper::dump(generator);
+   std::cout << "*********************" << std::endl;
+}
+
+void fileString()
+{
+   const std::string f =
+      "function foo0( ) { if (x < '}}}') { x+y; x+=1;} else {x;} }          "
+      "function foo1(x) { if (x < '}}}') { x+y; x+=1;} else {x;} }          "
+      "function foo2(x,y) { if (x < '}}}') { x+y; x+=1;} else {x;} }        "
+      "function foo3(x,y,z) { if (x < '}}}') { x+y; x+=1;} else {x;} }      "
+      "function foo4(x,y,z,w) { if (x < '}}}') { x+y; w+=1;} else {x; w;} } "
+      "function foo5( ) { if (x < '}}}') { x+y; x+=1;} else {x;} }          "
+      "function foo6(x) { if (x < '}}}') { x+y; x+=1;} else {x;} }          "
+      "function foo7(x,y) { if (x < '}}}') { x+y; x+=1;} else {x;} }        "
+      "function foo8(x,y,z) { if (x < '}}}') { x+y; x+=1;} else {x;} }      "
+      "function foo9(x,y,z,w) { if (x < '}}}') { x+y; w+=1;} else {x; w;} } "
+      "function foo10(x,y,z,w) { if (x < '\\'}\\'}\\'}\\'') { x+y; w+=1;} else {x; w;} } "
+      "function foox( )       {  } "
+      "function fooy(x)       {  } "
+      "function fooz(x,y)     {  } "
+      "function foow(x,y,z)   {  } "
+      "function foou(x,y,z,w) {  } "
+      "function main() { int c=0;c=c+1;print(c);}"
+      "{xxx + yyy + zzz {k / l} }  ";
+
+   function_definition fd;
+
+   std::string residual = f;
+
+   std::cout << "***** actual function parsing *****" << std::endl;
+
+   int function_count = 0;
+
+   do
+   {
+      if (parse_function_definition(residual,fd))
+      {
+         std::string vars;
+
+         for (std::size_t i = 0; i < fd.var_list.size(); ++i)
+         {
+            vars += fd.var_list[i] + ((i < fd.var_list.size() - 1) ? "," : "");
+         }
+
+         printf("Function[%02d]\n",function_count++);
+         printf("Name: %s\n"      ,fd.name.c_str() );
+         printf("Vars: (%s)\n"    ,vars.c_str()    );
+         printf("Body: \n%s\n"    ,fd.body.c_str() );
+         parsingBody(fd.body.c_str());
+         printf("-----------------------------\n\n");
+
+         fd.clear();
+      }
+      else
+         break;
+   }
+   while (!residual.empty());
+
+   if (!residual.empty())
+   {
+      printf("Residual: %s\n",residual.c_str());
+   }
+
+   std::cout << "*********************" << std::endl;
+}
+
+
+int main()
+{
+   
+   fileString();
 
    return 0;
 }
